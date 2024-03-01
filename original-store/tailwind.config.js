d53270dc91b1292a2333e76982995002b98d6446
Ahmad-Haymour <92795077+Ahmad-Haymour@users.noreply.github.com>
2024-03-01 05:57:53 +0100

    initial commit

--- original-store/tailwind.config.js
+++ original-store/tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        // primary: "#81689D",
+        primary: "	#a46ede",
+        secondary: "#474F7A",
+      }
     },
   },
   plugins: [],
